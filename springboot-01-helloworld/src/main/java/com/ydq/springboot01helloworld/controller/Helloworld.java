291653d0da2c96da107df82a416ac1bf2be44226
devin <460953643@qq.com>
2020-05-13 14:25:56 +0800

    Helloworld demo

---
+++ springboot-01-helloworld/src/main/java/com/ydq/springboot01helloworld/controller/Helloworld.java
@@ -0,0 +1,12 @@
+package com.ydq.springboot01helloworld.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Helloworld {
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello world, springboot!";
+    }
+}
